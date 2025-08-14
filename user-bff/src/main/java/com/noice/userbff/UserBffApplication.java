2b60160e9a29ae635f40640b0a93ca8f94989d98
ADITYA <adityad@beepermd.com>
2025-08-14 11:22:52 +0000

    user bff service added

---
+++ user-bff/src/main/java/com/noice/userbff/UserBffApplication.java
@@ -0,0 +1,13 @@
+package com.noice.userbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserBffApplication.class, args);
+    }
+
+}
