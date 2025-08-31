d0d96b44eed47ad8c17b6057914b549d1cc1551e
ADITYA <adityad@beepermd.com>
2025-08-31 08:28:30 +0000

    product service

---
+++ product-bff/src/main/java/com/noice/productbff/ProductBffApplication.java
@@ -0,0 +1,13 @@
+package com.noice.productbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductBffApplication.class, args);
+    }
+
+}
--- user-bff/src/main/java/com/noice/userbff/controller/PermissionController.java
+++ user-bff/src/main/java/com/noice/userbff/controller/PermissionController.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
@@ -16,7 +15,6 @@ import java.util.Set;
 @RestController
 @RequestMapping("/permission")
 @AllArgsConstructor
-@Validated
 public class PermissionController {
 
     private PermissionService permissionService;
