7cda4c39d4b1fc63eed637dfd25874acde474cd9
ADITYA <adityad@beepermd.com>
2025-09-01 10:13:56 +0000

    configure product with jwt

---
+++ product-bff/src/main/java/com/noice/productbff/controller/ProductController.java
@@ -0,0 +1,22 @@
+package com.noice.productbff.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @GetMapping("/unauth")
+    public String unAuth(){
+        return "unAuth";
+    }
+
+    @GetMapping("/auth")
+    public String Auth() {
+        return "Auth";
+    }
+
+
+}
